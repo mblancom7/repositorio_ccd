e5357485d0318a985b183235bdba4cfb59182763
Marco Antonio Blanco Martinez <mblancom7@gmail.com>
2026-02-13 20:19:01 -0500

    Se agrego la funcion Restar2

--- Function.py
+++ Function.py
@@ -2,8 +2,8 @@ def sumar(a,b):
     return a+b
 
 
-def resta(a,b)
+def resta(a,b):
     return a-b
 print(f"la suma es {sumar(3,8)}")
-print(f"la suma es {resta(11,4)}")
+print(f"la resta es {resta(11,4)}")
 
